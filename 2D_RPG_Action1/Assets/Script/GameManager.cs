717207a2a28fee39a42758a043123b41872ef126
makiable <makiable@gmail.com>
2015-07-28 23:20:48 +0900

    122페이지 까지 진행.

---
+++ 2D_RPG_Action1/Assets/Script/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public ArcherControl mArcher; //아처 클래스를 가져온다.
+	[HideInInspector]
+	public List<MonsterControl> mMonster;
+
+	//오토 타깃이 된 몬스터를 참조합니다.
+	[HideInInspector]
+	public MonsterControl TargetMonster;
+
+	//몬스터 프리펩들을 인스턴스화 할 위치 정보입니다.
+	public Transform[] mSpawnPoint;
+
+	//던전을 탐험하는 횟수입니다.
+	private int mLoopCount = 5;
+
+	//화면에 나타난 적의 합
+	private int mMonsterCount = 0;
+
+	//얼마만큼 뛰다가 적을 만날 것인지.
+	private float mRunTime = 1.8f;
+
+	// Use this for initialization
+	void Start () {
+		//적 몬스터들이 담길 List
+		mMonster = new List<MonsterControl> ();
+		mMonster.Clear ();
+
+		//던전 탐험 스텝을 만들어서 순서대로 순환 시킵니다.
+		StartCoroutine ("AutoStep");
+	}
+	
+	//던전의 현재 스텝
+	public enum Status{
+		Idle,
+		Run,
+		BattleIdle,
+		Battle,
+		Clear
+	}
+
+	public Status mStatus = Status.Idle;
+
+	//오토스탭을 자동으로 실행.
+	IEnumerator AutoStep()
+	{
+		while (true) { //코루틴이 계속 실행 될때..
+			if (mStatus == Status.Idle) { //아이들 상태라면.
+				yield return new WaitForSeconds (1.2f); //1.2초후 던전탐험을 시작..
+				mStatus = Status.Run; //런상태로..
+			} else if (mStatus == Status.Run) {
+				//아처의 애니메이션 상태를 달리기로 설정합니다.
+				mArcher.SetStatus (ArcherControl.Status.Run, 1);
+
+				//mRunTime 후 배틀 대기 상태로 돌입합니다.
+				yield return new WaitForSeconds (mRunTime);
+				mStatus = Status.BattleIdle;
+			}
+
+
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 2D_RPG_Action1/Assets/Script/MonsterControl.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterControl : MonoBehaviour {
+
+	public Animator mAnimator; //애니메이터를 사용할껍니다. mAnimator에 들어감.
+
+	//생성될 몬스터의 인덱스, 체력, 공격력, 공겨속도 선언.
+	[HideInInspector]
+	public int idx;
+	public int mHP;
+	public int mAttack;
+	public float mAttackSpeed;
+
+	//몬스터가 발사할 파이어볼의 발사지점
+	public Transform mFireShootSpot;
+
+	//몬스터 피격 설정을 위한 콜라이더.
+	public Collider mCollider;
+
+	//몬스터가 사용할 파이어 볼 프리팹
+	private Object mFirePrefab;
+
+	//몬스터의 상태.
+	public enum Status
+	{
+		Alive,
+		Dead
+	}
+
+	[HideInInspector]
+	public Status mStatus = Status.Alive; //기본은 살아 있도록.
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	//생성되는 몬스터는 현재 체력의 +-10의 채력을 가지게 됩니다.
+	public void RandomHP(){
+		mHP += Random.Range (-10, 10);
+	}
+
+	//몬스터들이 오토 타게팅이 될 경우만 콜라이더를 설정하게 합니다.
+	public void SetTarget(){
+		mCollider.enabled = true;
+	}
+
+	//피격 당할 경우 데미지 처리와 애니메이션 처리
+	public void Hit(){
+		mAnimator.SetTrigger ("Damage");
+
+		//사망처리
+		if (mHP <= 0) {
+			mStatus = Status.Dead;
+			mHP = 0;
+			mCollider.enabled = false; //콜라이더 처리는 빼고.
+			mAnimator.SetTrigger("Die");
+		}
+	}
+
+	//파이어볼 프리팹을 인스턴스 화해서 사용합니다.
+	private void ShootFire(){
+	
+			//파이어볼 프리펩을 씬에서 인스턴스화 하는 과정을 작성하게 됩니다.
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
